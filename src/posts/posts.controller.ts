f4ea19b1db814a4104959397dca27067144607b3
Ryu-02 <ymq02110192@gmail.com>
2025-05-23 11:45:01 +0900

    refactor: (#19) 서비스 응답 책임 컨트롤러로 이동 및 userName → name 필드 수정

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -22,49 +22,77 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  createPost(
+  async createPost(
     @Param('groupId', ParseIntPipe) groupId: number,
     @Body() createPostDto: CreatePostDto,
     @Req() req: Request,
   ) {
     const user = req.user as { userId: number };
     const userId = user.userId;
-    return this.postsService.createPost(createPostDto, groupId, userId);
+    const post = await this.postsService.createPost(
+      createPostDto,
+      groupId,
+      userId,
+    );
+    return {
+      status: 'success',
+      message: '게시물이 성공적으로 생성되었습니다.',
+      data: post,
+    };
   }
 
   @Get()
-  getAllPosts(@Param('groupId', ParseIntPipe) groupId: number) {
-    return this.postsService.getAllPosts(groupId);
+  async getAllPosts(@Param('groupId', ParseIntPipe) groupId: number) {
+    const posts = await this.postsService.getAllPosts(groupId);
+    return {
+      status: 'success',
+      message: '게시물 목록을 성공적으로 가져왔습니다.',
+      data: posts,
+    };
   }
 
   @Get(':postId')
-  getPostById(
-    @Param('groupId', ParseIntPipe) groupId: number,
-    @Param('postId', ParseIntPipe) postId: number,
-  ) {
-    return this.postsService.getPostById(postId);
+  async getPostById(@Param('postId', ParseIntPipe) postId: number) {
+    const post = await this.postsService.getPostById(postId);
+    return {
+      status: 'success',
+      message: '게시물을 성공적으로 가져왔습니다.',
+      data: post,
+    };
   }
 
   @Patch(':postId')
-  updatePost(
-    @Param('groupId', ParseIntPipe) groupId: number,
+  async updatePost(
     @Param('postId', ParseIntPipe) postId: number,
     @Body() updatePostDto: UpdatePostDto,
     @Req() req: Request,
   ) {
     const user = req.user as { userId: number };
     const userId = user.userId;
-    return this.postsService.updatePost(updatePostDto, postId, userId);
+    const updatedPost = await this.postsService.updatePost(
+      updatePostDto,
+      postId,
+      userId,
+    );
+    return {
+      status: 'success',
+      message: '게시물이 성공적으로 수정되었습니다.',
+      data: updatedPost,
+    };
   }
 
   @Delete(':postId')
-  deletePost(
-    @Param('groupId', ParseIntPipe) groupId: number,
+  async deletePost(
     @Param('postId', ParseIntPipe) postId: number,
     @Req() req: Request,
   ) {
     const user = req.user as { userId: number };
     const userId = user.userId;
-    return this.postsService.deletePost(postId, userId);
+    await this.postsService.deletePost(postId, userId);
+    return {
+      status: 'success',
+      message: '게시물이 성공적으로 삭제되었습니다.',
+      data: null,
+    };
   }
 }
