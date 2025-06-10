530f4c20156d41c39c6abd075cffd5dd1a4dd9bb
Ryu-02 <ymq02110192@gmail.com>
2025-06-10 13:54:03 +0900

    fix: (#25) update-post.dto 초기화 오류 수정

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,18 +1,4 @@
-import { PartialType, ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @ApiProperty({
-    description: '게시물 제목 (수정 가능)',
-    example: '수정된 제목입니다.',
-    required: false,
-  })
-  title?: string;
-
-  @ApiProperty({
-    description: '게시물 내용 (수정 가능)',
-    example: '수정된 내용입니다.',
-    required: false,
-  })
-  content?: string;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
