fcf8ad86f2633c527d93b0eabcf0954e4aa7c958
Ryu-02 <ymq02110192@gmail.com>
2025-09-30 09:42:33 +0900

    refactor: (#144) aws GroupBanner 폴더명 변경

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -31,7 +31,7 @@ export class S3Service {
   private readonly folderMap: Record<S3ObjectType, string> = {
     [S3ObjectType.PROFILE]: 'profile/user_uploads/',
     [S3ObjectType.GROUP]: 'group/',
-    [S3ObjectType.GROUP_BANNER]: 'groupBanner/',
+    [S3ObjectType.GROUP_BANNER]: 'group_banner/',
     [S3ObjectType.POST]: 'post/',
   };
 
@@ -77,7 +77,7 @@ export class S3Service {
       case S3ObjectType.PROFILE:
         return `profile/${uploadContext.userId}/${uniqueId}${fileExtension}`;
       case S3ObjectType.GROUP_BANNER:
-        return `groupBanner/${uploadContext.groupId}/${uniqueId}${fileExtension}`;
+        return `group_banner/${uploadContext.groupId}/${uniqueId}${fileExtension}`;
       default:
         throw new InternalServerErrorException(
           '지원하지 않는 업로드 컨텍스트입니다.',
